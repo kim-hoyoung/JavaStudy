c12d28594ffbe4eefe9bb5cd70db11607702fe59
kim-hoyoung <happy3sk@naver.com>
2024-06-19 11:10:38 +0900

    other place commit

---
+++ src/cond/ex/GradeSwitchEx.java
@@ -0,0 +1,33 @@
+package cond.ex;
+
+public class GradeSwitchEx {
+    public static void main(String[] args) {
+        String grade = "A";
+
+        switch (grade) {
+            case "A":
+                System.out.println("탁월한 성과입니다");
+                break;
+            case "B":
+                System.out.println("탁월한 성과입니다");
+                break;
+            case "C":
+                System.out.println("탁월한 성과입니다");
+                break;
+            case "D":
+                System.out.println("탁월한 성과입니다");
+                break;
+            case "E":
+                System.out.println("탁월한 성과입니다");
+                break;
+            case "F":
+                System.out.println("불합격입니다");
+
+            default:
+                System.out.println("잘못된 학점입니다.");
+
+
+
+        }
+    }
+}

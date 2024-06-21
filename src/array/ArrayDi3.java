0e8c99df052bb57ddc538b3f70e648eb67a2be7e
kim-hoyoung <happy3sk@naver.com>
2024-06-21 14:24:36 +0900

    good lunch

---
+++ src/array/ArrayDi3.java
@@ -0,0 +1,21 @@
+package array;
+
+public class ArrayDi3 {
+    public static void main(String[] args) {
+        //2X3 2차원 배열을 만든다.
+        int[][] arr = {
+                {1,2,3},
+                {4,5,6}
+        };
+        //행 2, 열 3
+
+
+
+        for (int row = 0; row < arr.length; row++) {
+            for (int column = 0; column < arr[row].length; column++){
+                System.out.print(arr[row][column] + " "); //8열 출력
+            }
+            System.out.println();
+        }
+    }
+}

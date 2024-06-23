b3165ed09c964429c91a50ddec2de54b817ba27c
kim-hoyoung <your_email@example.com>
2024-06-23 18:51:18 +0900

    min

---
+++ src/array/codetree/min.java
@@ -0,0 +1,26 @@
+package array.codetree;
+
+import java.util.Scanner;
+
+public class min {
+        public static void main(String[] args) {
+
+            Scanner input = new Scanner(System.in);
+
+            int num1 = input.nextInt();
+            int num2 = input.nextInt();
+            int num3 = input.nextInt();
+
+            int min1 = (num2 >= num1 && num1 <= num3) ? 1 : 0;
+            int min2 = (num1 >= num2 && num2 <= num3) ? 1 : 0;
+            int min3 = (num1 >= num3 && num3 <= num2) ? 1 : 0;
+            if(min1 == 1) {
+                System.out.print(num1);
+            } else if (min2 == 1) {
+                System.out.print(num2);
+            } else if (min3 == 1) {
+                System.out.print(num3);
+            }
+        }
+}
+
